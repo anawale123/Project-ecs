761fbccf96f7c9cc8a96504a45c405a4760a46d4
Anas awale <32065533+anawale123@users.noreply.github.com>
2026-04-08 01:22:37 +0100

    Update ecs-service.tf

--- terraform/modules/ecs/ecs-service.tf
+++ terraform/modules/ecs/ecs-service.tf
@@ -1,36 +1,36 @@
 # ECS SERVICE (UMAMI)
-
 resource "aws_ecs_service" "umami-service" {
   name            = "umami-ecs-service"
   cluster         = aws_ecs_cluster.umami_app.id
   task_definition = aws_ecs_task_definition.umami_task_def.arn
   desired_count   = 2
-  launch_type      = "FARGATE"
+  launch_type     = "FARGATE"
 
+  # DEPLOYMENT CONTROLLER
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
 
-
-# NETWORK CONFIGURATION 
+  # NETWORK CONFIGURATION
   network_configuration {
     subnets          = var.private_subnets
     assign_public_ip = false
-    security_groups = [var.ecs_sg_id]
-
+    security_groups  = [var.ecs_sg_id]
   }
 
-# LOAD BALANCER CONFIGURATION
+  # LOAD BALANCER CONFIGURATION
   load_balancer {
     target_group_arn = var.target_group_arn
-    container_name = "umami"
-    container_port = 3000
-
+    container_name   = "umami"
+    container_port   = 3000
   }
-# ECS SERVICE TASK DEFINITION
+
+  lifecycle {
+    ignore_changes = [task_definition, load_balancer]
+  }
+
+  # ECS SERVICE TASK DEFINITION
   depends_on = [
     aws_ecs_task_definition.umami_task_def
   ]
-
 }
-# CODE DPELOY 
- deployment_controller {
-    type = "CODE_DEPLOY"
-  }
